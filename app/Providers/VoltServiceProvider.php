8036c4cd29bdabf1e73228c7182b863f88eb09ca
Mat Santos <miliandroxd@gmail.com>
2024-04-03 17:11:46 -0300

    setup

--- app/Providers/VoltServiceProvider.php
+++ app/Providers/VoltServiceProvider.php
@@ -7,22 +7,10 @@ use Livewire\Volt\Volt;
 
 class VoltServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
         Volt::mount([
             config('livewire.view_path', resource_path('views/livewire')),
-            resource_path('views/pages'),
         ]);
     }
 }
